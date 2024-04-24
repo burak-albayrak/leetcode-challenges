22bde813a5f2c3ee42abd2f83c139655f26049bb
Burak Albayrak <burak_albayrak0@icloud.com>
2024-04-24 12:09:25 +0300

    Create CanPlaceFlowers.cs

---
+++ 605.Can-Place-Flowers/CanPlaceFlowers.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.Length && n > 0; i++)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) 
+            && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) 
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n == 0;
+    }
+}
