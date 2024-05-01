8c9da5b05ae870a68bccb54f7037fb306ae520ac
Burak Albayrak <burak_albayrak0@icloud.com>
2024-05-01 22:36:12 +0300

    Create RemoveElement.cs

---
+++ 27.Remove-Element/RemoveElement.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
