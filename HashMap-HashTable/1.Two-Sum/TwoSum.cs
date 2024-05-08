23b947ae54330941a6e811feb5fd168805bb0c85
Burak Albayrak <burak_albayrak0@icloud.com>
2024-05-08 20:59:10 +0300

    Create TwoSum.cs

---
+++ HashMap-HashTable/1.Two-Sum/TwoSum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    Dictionary<int, int> numMap = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++) 
+    {
+        int complement = target - nums[i];
+        if (numMap.ContainsKey(complement)) 
+        {
+            return new int[] {numMap[complement], i};
+        }
+
+        numMap[nums[i]] = i;
+    }
+        
+    return null;
+    }
+}
