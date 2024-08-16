0a8b7dd754543e04da68db246b48c1ef5bf4c2f0
Burak Albayrak <burak_albayrak0@icloud.com>
2024-08-16 12:33:18 +0300

    Create removeElements.swift

---
+++ LinkedList/203.Remove-Linked-List-Elements/removeElements.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+ 
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+    let dummy = ListNode(0)
+    dummy.next = head
+    var current = dummy
+    
+    while current.next != nil {
+        if current.next?.val == val {
+            current.next = current.next?.next
+        } else {
+            current = current.next!
+        }
+    }
+    
+    return dummy.next
+    }
+}
